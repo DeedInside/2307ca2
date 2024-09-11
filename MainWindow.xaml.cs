d75064df3790fad9f27acf447d682070ecb70ec2
Пауль Сергей Александрович <paul_s_a@mail.ru>
2024-09-11 10:15:57 +0700

    18.0911.09

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,105 @@
-﻿using System.Windows;
+﻿using _2307ca2.Model;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace _2307ca2
 {
     public partial class MainWindow : Window
     {
+        List<User> users;
         public MainWindow()
         {
             InitializeComponent();
+            
+            users = new List<User>()
+            {
+                new User("name 1", "email", "pass"),
+                new User("name 2", "email", "pass"),
+                new User("name 3", "email", "pass"),
+            };
+            ListBoxUser.ItemsSource = users;
+            ComboBoxUserName.ItemsSource = ConverUserToName(users);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button bt = sender as Button;
+            bt.Content = "OK";
+            bt.FontSize = 40;
+
+            tBlock.Text = tBox.Text;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox ch = sender as CheckBox;
+
+            if (ch.IsChecked == true)
+            {
+                StackPanelInpyt.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox ch = sender as CheckBox;
+
+            if (ch.IsChecked == false)
+            {
+                StackPanelInpyt.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            
+            if(rb.IsChecked == true)
+            {
+                if(rb.Content.ToString() == "выбор 1")
+                {
+                    MessageBox.Show("верное решение " + rb.Content.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("не верное решение " + rb.Content.ToString());
+                }
+            }
+        }
+
+        private void ListBoxUser_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox list = sender as ListBox;
+
+            MessageBox.Show(list.SelectedItem.ToString());
+        }
+
+        private void Add_user(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User("new user", "new email", "new pass"));
+            ListBoxUser.Items.Refresh();
+        }
+
+        private void selected_user_name(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox box = sender as ComboBox;
+
+            MessageBox.Show(box.SelectedItem.ToString());
+        }
+        public List<string> ConverUserToName(List<User> user)
+        {
+            List<string> result = new List<string>();
+
+            foreach (var item in user)
+            {
+                result.Add(item.Name);
+            }
+            return result;
         }
     }
 }
---
+++ Model/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2307ca2.Model
+{
+    public class User
+    {
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Date {  get; set; } = DateTime.Now;
+
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Password}";
+        }
+
+    }
+}
